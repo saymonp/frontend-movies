c315582e612414ca8159183f035bda41a052649d
Saymon <saymon.T@outlook.com>
2026-05-07 20:24:12 -0300

    Feat: Notificações

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,16 @@ import i18n from './i18n/index'
 
 import App from './App.vue'
 import router from './router'
+import Toast from "vue-toastification";
+import "vue-toastification/dist/index.css";
+
+
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.use(i18n)
+app.use(Toast);
 
 app.mount('#app')
